99f2441cd4c33058ec6778a653fd2cd64598de60
thesaintzion <saintzion777@gmail.com>
2020-07-03 15:10:11 +0100

    Updates

---
+++ precache-manifest.29d1409d4e424051153f168e3ee5d2c9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb99501fba4d6e12cccb70d0e04fa392",
+    "url": "/react-h-v/index.html"
+  },
+  {
+    "revision": "0bc760aa901daef4e685",
+    "url": "/react-h-v/static/css/2.ace36aee.chunk.css"
+  },
+  {
+    "revision": "b1704e6893fd3372a70f",
+    "url": "/react-h-v/static/css/4.26224dba.chunk.css"
+  },
+  {
+    "revision": "9ff320c49b1826985e09",
+    "url": "/react-h-v/static/css/5.286e2b22.chunk.css"
+  },
+  {
+    "revision": "052a508e6307903813ac",
+    "url": "/react-h-v/static/css/main.380c45d6.chunk.css"
+  },
+  {
+    "revision": "0bc760aa901daef4e685",
+    "url": "/react-h-v/static/js/2.5fca0935.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/react-h-v/static/js/2.5fca0935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984f2c402ae64e7530b7",
+    "url": "/react-h-v/static/js/3.a8ae7cb0.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/react-h-v/static/js/3.a8ae7cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1704e6893fd3372a70f",
+    "url": "/react-h-v/static/js/4.7b9cc2b9.chunk.js"
+  },
+  {
+    "revision": "9ff320c49b1826985e09",
+    "url": "/react-h-v/static/js/5.c4e3f355.chunk.js"
+  },
+  {
+    "revision": "052a508e6307903813ac",
+    "url": "/react-h-v/static/js/main.612a1491.chunk.js"
+  },
+  {
+    "revision": "8c91db4280fc8df5cff8",
+    "url": "/react-h-v/static/js/runtime-main.ec003fcf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-h-v/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-h-v/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-h-v/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-h-v/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-h-v/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "99a762fb250da88037468170b6e37f40",
+    "url": "/react-h-v/static/media/home-search-bg.99a762fb.jpg"
+  },
+  {
+    "revision": "a2b809e39de6b4947e9f1eb59fc7c610",
+    "url": "/react-h-v/static/media/testimonial-bg-1.a2b809e3.jpg"
+  }
+]);
